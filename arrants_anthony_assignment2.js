e729e227a29cb9bebb6f38369351def08f0e335b
Anthony Arrants <antnya@fullsail.edu>
2014-04-10 18:13:49 -0400

    problems with the prompt.

--- arrants_anthony_assignment2.js
+++ arrants_anthony_assignment2.js
@@ -6,12 +6,27 @@
 
 // declaring global variables for my code
 
-var criminal1 = Donovan;
-var criminal2 = Derek;
-var officer1 = Butch;
-var officer2 = Cuonzo;
+var criminal1 = "Donovan";
+var criminal2 = "Derek";
+var officer1 = "Butch";
+var officer2 = "Cuonzo";
 numberOfATMS = 2;
 cameraOnATM = true;
 
 // Prompt so I can return a value to my procedure
+
+var getawayPrompt;
+
+getawayPrompt = prompt("Do we have a getaway car?");
+
+if (getawayPrompt === yes) {
+        
+    console.log("We have a getaway car, lets go hit those ATMs!");
+    
+} else { 
+        
+    console.log("We need to steal a car!");
+        
+}
+
 
