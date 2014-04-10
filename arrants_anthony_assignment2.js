15e076bb8e3cb7d68cf56b3df54b174e78f2ff24
Anthony Arrants <antnya@fullsail.edu>
2014-04-10 17:51:41 -0400

    Defined local variables.

--- arrants_anthony_assignment2.js
+++ arrants_anthony_assignment2.js
@@ -4,5 +4,14 @@
 // Project 2
 // Is this FUNCTIONal? Fun with functions in JavaScript.
 
-console.log("Woohoo!");
+// declaring global variables for my code
+
+var criminal1 = Donovan;
+var criminal2 = Derek;
+var officer1 = Butch;
+var officer2 = Cuonzo;
+numberOfATMS = 2;
+cameraOnATM = true;
+
+// Prompt so I can return a value to my procedure
 
