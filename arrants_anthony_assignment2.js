4ec99dee25579a366f18e6ef3ed5dedadfafc57c
Anthony Arrants <antnya@fullsail.edu>
2014-04-10 22:01:11 -0400

    Maybe I finally understand. Got my functions constructed and called. Now I want to fill out with some console logs to make a coherent story.

--- arrants_anthony_assignment2.js
+++ arrants_anthony_assignment2.js
@@ -4,30 +4,58 @@
 // Project 2
 // Is this FUNCTIONal? Fun with functions in JavaScript.
 
-// declaring global variables for my code
+// Prompt and procedure
 
-var criminal1 = "Donovan";
-var criminal2 = "Derek";
-var officer1 = "Butch";
-var officer2 = "Cuonzo";
-numberOfATMS = 2;
-cameraOnATM = true;
-
-// Prompt so I can return a value to my procedure
+var moneyNeeded = prompt("How much money do we need?" , "Enter a value less than 1000.");
 
 
-var getawayPrompt;
-
-getawayPrompt = prompt("Do we have a getaway car?");
-
-if (getawayPrompt === "yes") {
-        
-    console.log("We have a getaway car, lets go hit those ATMs!");
-    
-} else { 
-        
-    console.log("We need to steal a car!");
-        
+if(parseInt(moneyNeeded) < 20000) {
+    console.log("There will be plenty of money in the ATM.");    
+} else {
+    console.log("We need to figure out a different place to hit.")
 }
+
+
+// confirmation going into a Boolean function. 
+
+var booleanFunction = function(readyToGo, notReady) {
+    while (notReady) {
+        readyToGo = confirm("Are you ready to go?");
+        if (readyToGo){
+            console.log("Let's go.");    
+            notReady = false;
+        }
+        else console.log("Still not ready.") 
+    }
+};
+
+booleanFunction(false, true);
+
+
+
+// Number function
+
+var ATM = function(number) {
+  var i;
+  for(i = number; i >= 25; i-=25) {
+    console.log("Removing money, "+i+" "+ "dollars" + " left");
+    }
+    return i;
+};
+
+ATM(moneyNeeded); 
+
+
+// strings go here.
+var concat = function(string1, string2) {
+    var returnString = string1 + " " + string2;
+    return returnString; 
+};
+
+console.log(concat("We are going to get in trouble" , "or so I think."));
+
+
+
+
 
 
