8f1e41dc25df4f9d7cb5f8576aabd5fc5ff693f8
Anthony Arrants <antnya@fullsail.edu>
2014-04-10 23:09:04 -0400

    I guess I am going to throw in the towel. I got some console logs in here so my story was somewhat coherent.

--- arrants_anthony_assignment2.js
+++ arrants_anthony_assignment2.js
@@ -4,6 +4,18 @@
 // Project 2
 // Is this FUNCTIONal? Fun with functions in JavaScript.
 
+// Global variables
+
+var crook1 = "Butch";
+var readyToGo = true;
+
+
+console.log("I think it's time to rob a bank. I am bored and want to have fun, but I am broke.");
+console.log("I am going to get my friend" + crook1 + " to help me knock off two ATMs outside this bank down the road.");
+console.log("First, lets find out how much money we need in order to have a fair amount of fun.");
+console.log("We are worried that the ATMs may not have enough cash in them to supply our need.");
+console.log("I think there should be more than $20,000 in the ATM. Is there a way to check?");
+console.log("Butch hacked into the bank to check the status of the ATM.");
 // Prompt and procedure
 
 var moneyNeeded = prompt("How much money do we need?" , "Enter a value less than 1000.");
@@ -31,8 +43,13 @@ var booleanFunction = function(readyToGo, notReady) {
 
 booleanFunction(false, true);
 
-
-
+console.log("I have never been so nervous to do something in my life, I told Butch.");
+console.log("Butch said: If we get arrested, you better not rat me out.");
+console.log("I won't I said.");
+console.log("We approach the bank, take a deep breath, and start walking to our respective ATMs.");
+console.log("At nearly the exact same time, Butch and I pull our computers out and start pecking away as we try to interface with the ATMs.");
+console.log("We both look at eachother--we have hit the jackpot. Now we have to wait for the money to shoot out.");
+console.log("I hear the inner workings of the ATM moving as money starts to spit out.");
 // Number function
 
 var ATM = function(number) {
@@ -45,6 +62,8 @@ var ATM = function(number) {
 
 ATM(moneyNeeded); 
 
+console.log("Holy, I just saw the police drive by on the road above us! Let's go!");
+console.log("We get to the car and start driving to a hideout.");
 
 // strings go here.
 var concat = function(string1, string2) {
@@ -53,7 +72,9 @@ var concat = function(string1, string2) {
 };
 
 console.log(concat("We are going to get in trouble" , "or so I think."));
-
+console.log("\"Maybe we won't\", I tell Butch. For now, lets just try to have some fun until we get caught.");
+console.log("Let's pop some some bottles.");
+console.log("The end.");
 
 
 
